f6de77f3ff347d1c60ff90ed81af67cc6f642a6b
Shi <45276858+Syk27syk@users.noreply.github.com>
2022-05-12 16:08:26 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// empty javascript file
+
+const testObject = {
+    name: "test object",
+    color: "green",
+    numericProperty: {
+        one: 1,
+        two: 2,
+    },
+    bool: false,
+}
+
+const testFunction = () => {
+    let a = 0;
+    a++
+    return a
+}
